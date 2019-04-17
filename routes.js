c134ae578916a4cb55972ff0eaeadb5d0ebda84a
Adalberto Taylor <adalbertotaylor84@gmail.com>
2019-04-17 00:07:26 -0300

    Setup now deployment

--- routes.js
+++ routes.js
@@ -1,6 +1,5 @@
 const nextRoutes = require('next-routes');
 const routes = (module.exports = nextRoutes());
 
-routes.add('home', '/');
 routes.add('random', '/random/:category?');
 routes.add('joke', '/joke/:joke');
